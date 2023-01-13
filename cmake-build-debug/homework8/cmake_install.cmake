af70341257e8099041dadc447533528f940ed21f
salieri <1483224757@qq.com>
2023-01-13 13:10:59 +0800

    test_git_commit

--- cmake-build-debug/homework8/cmake_install.cmake
+++ cmake-build-debug/homework8/cmake_install.cmake
@@ -34,6 +34,6 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "E:/Software/Toolbox/apps/CLion/ch-0/222.4167.35/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "E:/Software/Toolbox/apps/CLion/ch-0/223.8214.51/bin/mingw/bin/objdump.exe")
 endif()
 
---
+++ test_git.c
@@ -0,0 +1,10 @@
+//
+// Created by 14832 on 2023/1/13.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("hello");
+    return 0;
+}
